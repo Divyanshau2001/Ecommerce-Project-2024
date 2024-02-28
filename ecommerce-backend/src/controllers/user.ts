1bb04ad433fb89309d041f35af180016f5cbb8f1
unknown <divyanshnshu2001@gmail.com>
2024-02-28 22:41:02 +0530

    started working on controllers

---
+++ ecommerce-backend/src/controllers/user.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import { User } from "../models/user.js";
+import { NewUserRequestBody } from "../types/types.js";
+
+
+export const newUser = async(
+    req: Request,
+    res: Response, 
+    next: NextFunction) => {
+    try {
+        const {name, email, photo, gender, role, _id, dob} = req.body;
+        const user = await User.create({name, email, photo, gender, role, _id, dob});
+
+        res.status(200).json({
+            success: true,
+            message: `Welcome , ${user.name}`,
+        })
+    }
+    catch(error) {}
+};
