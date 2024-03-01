d5d45f53b8c28e0b17c79ba6a620b32124528f39
unknown <divyanshnshu2001@gmail.com>
2024-03-01 22:38:25 +0530

    DB connections established

--- ecommerce-backend/dist/app.js
+++ ecommerce-backend/dist/app.js
@@ -1,6 +1,19 @@
 import express from 'express';
-const port = 3000;
+import { connectDB } from './utils/features.js';
+//importing routes
+import userRoute from './routes/user.js';
+const port = 4000;
+connectDB();
 const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("API is working with  /api/v1");
+});
+//using routes
+app.use("/api/v1/user", userRoute);
+// app.use((err:Error,req:Request,res: Response,next: NextFunction) => {
+//     return res
+// }) 
 app.listen(port, () => {
     console.log(`Express is working on localhost http://localhost:${port}`);
 });
--- ecommerce-backend/src/types/types.ts
+++ ecommerce-backend/src/types/types.ts
@@ -1,6 +1,6 @@
 
 
-
+    
 export interface NewUserRequestBody{
     name: string;
     email: string;
