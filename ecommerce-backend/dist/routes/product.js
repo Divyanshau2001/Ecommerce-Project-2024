bf9f3552e2cc665d01c7b52d44f52851ad195176
unknown <divyanshnshu2001@gmail.com>
2024-03-17 01:34:51 +0530

    changes

--- ecommerce-backend/dist/routes/product.js
+++ ecommerce-backend/dist/routes/product.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { adminOnly } from "../middleware/auth.js";
-import { getlatestProducts, newProduct } from "../controllers/product.js";
+import { deleteProduct, getAdminProducts, getAllCategories, getSingleProduct, getlatestProducts, newProduct, updateProduct } from "../controllers/product.js";
 import { singleUpload } from "../middleware/multer.js";
 const app = express.Router();
+//Create new Product   api/v1/product/new
 app.post("/new", adminOnly, singleUpload, newProduct);
-app.get("/lastest", getlatestProducts);
+app.get("/latest", getlatestProducts);
+app.get("/categories", getAllCategories);
+app.get("/admin-products", getAdminProducts);
+app.route("/:id").get(getSingleProduct).put(singleUpload, updateProduct).delete(deleteProduct);
 export default app;
--- ecommerce-backend/src/routes/product.ts
+++ ecommerce-backend/src/routes/product.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import { newUser, getAllUsers, getUser, deleteUser } from "../controllers/user.js";
 import { adminOnly } from "../middleware/auth.js";
-import { getlatestProducts, newProduct } from "../controllers/product.js";
+import { deleteProduct, getAdminProducts, getAllCategories, getSingleProduct, getlatestProducts, newProduct, updateProduct } from "../controllers/product.js";
 import { singleUpload } from "../middleware/multer.js";
 
 const app = express.Router();
 
+//Create new Product   api/v1/product/new
 app.post("/new",adminOnly ,singleUpload , newProduct);
 
-app.get("/lastest",getlatestProducts);
+app.get("/latest",getlatestProducts);
+
+app.get("/categories",getAllCategories);
+
+app.get("/admin-products",getAdminProducts);
+
+app.route("/:id").get(getSingleProduct).put(singleUpload,updateProduct).delete(deleteProduct);
 
 export default app;
